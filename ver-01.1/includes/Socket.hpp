5e878db86207fd3f298b92a72d7bea1feaccd270
Samih <sakmohamed@gmail.com>
2024-01-03 04:47:52 +0400

    feat(Socket): Initial Socket class implementation
    - still a `WIP` constructors and destructors are implemented
    - full set of methods to be implemented

--- ver-01.1/includes/Socket.hpp
+++ ver-01.1/includes/Socket.hpp
@@ -1,16 +1,23 @@
 #include <netinet/in.h>
 #include <netinet/ip.h> /* superset of previous */
+#include <sys/socket.h>
 
 // TODO
+// [ ] split-up function implementations into their own files
 // [ ] error checking functions for domain, type, protocol ... etc
 // [ ] custom exception class for error handling
 // [ ] better error messages
+// [ ] make a unit-tester main
+// [ ] add shutdown functionality (block recv, block send, close())
 
 #ifndef SOCKET_HPP
 #define SOCKET_HPP
 
 // error messages
-#define SOCKET_ERR "socket failed"
+#define ERR_NULL_MSG "Null ptr!!" // make more verbose
+#define ERR_STRT_MSG "Socket creation failed"
+#define ERR_BIND_MSG "Socket binding failed"
+#define ERR_LIST_MSG "Socket binding failed"
 
 // error codes
 enum WS_CODE
@@ -22,29 +29,45 @@ enum WS_CODE
 };
 
 // socket_descriptor type
-// cant use alias
 typedef int file_descriptor;
-
-// ports
-#define PORT_HTTP 80
 
 class Socket
 {
 
     public:
-        Socket(int domain, int type, int protocol);
-        ~Socket();
-        // member functions
-        WS_CODE bind(int family, int port, int address = INADDR_ANY);
+        // constructors
+        Socket(int domain, int type, int protocol = 0, int flags = SOCK_NONBLOCK);
+        Socket(int         domain,
+               int         type,
+               const char *protocol_name,
+               int         flags = SOCK_NONBLOCK);
+
+        // destructor
+        ~Socket(); // close vs shutdown
+
+        // member functions (API)
+        WS_CODE bind(int port);
+        // WS_CODE connect();
         WS_CODE listen(int backlog);
+        // WS_CODE accept();
+        // WS_CODE send(const char *msg);
+        // WS_CODE recv(const char *buff);
+        // WS_CODE getpeer(struct sockaddr &address);
+        // WS_CODE shutdown(int option);
 
     private:
         static const int invalid_file_descriptor = -1;
         file_descriptor  socket_descriptor;
 
+        int family; // domain of communicatoin
+        int type;   // type of socket
+
         // deleted but can't cause is 98 maguy
-        Socket(const Socket &);
-        Socket &operator=(const Socket &);
+        Socket(const Socket &){};
+        Socket &operator=(const Socket &)
+        {
+            return *this;
+        };
 };
 
 #endif // SOCKET_HPP
--- ver-01.1/srcs/Socket.cpp
+++ ver-01.1/srcs/Socket.cpp
@@ -1,5 +1,6 @@
 #include "Socket.hpp"
 #include <cerrno>
+#include <netdb.h>
 #include <netinet/in.h>
 #include <netinet/ip.h> /* superset of previous */
 #include <sstream>
@@ -8,24 +9,98 @@
 #include <sys/types.h> /* See NOTES */
 #include <unistd.h>
 
-Socket::Socket(int domain, int type, int protocol)
+#ifdef __DEBUG__
+#include <cassert>
+#include <iostream>
+#define DEBUGASSERT(x) assert(x)
+#else
+#define DEBUGASSERT(x)                                                                   \
+    do                                                                                   \
+    {                                                                                    \
+    } while (0)
+#endif // __DEBUG__
+
+/**
+ * Creates a new socket with the specified domain, type, protocol, and flags.
+ *
+ * @param domain The communication domain for the socket. This selects the protocol
+ * family.
+ * @param type The type of socket to be created.
+ * @param protocol The protocol to be used. By default, the protocol is set to 0.
+ * @param flags The flags to be set for the socket. By default, the flags are set to
+ * SOCK_NONBLOCK.
+ *
+ * @throws std::runtime_error if the socket creation fails.
+ *
+ * @note Normally, only one protocol exists to support a specific socket type within a
+ * particular protocol family.
+ */
+Socket::Socket(int domain, int type, int protocol, int flags)
+    : socket_descriptor(invalid_file_descriptor)
+    , family(domain)
+    , type(type)
+{
+    DEBUGASSERT(domain > 0);
+    DEBUGASSERT(protocol >= 0);
+
+    socket_descriptor = socket(domain, type | flags, protocol);
+
+    // On success, a file descriptor for the new socket is returned.
+    // On error, -1 is returned, and errno is set
+    if (socket_descriptor == invalid_file_descriptor)
+    {
+        std::stringstream errorMessage;
+        errorMessage << ERR_STRT_MSG << ": " << strerror(errno);
+        throw std::runtime_error(errorMessage.str());
+    }
+#ifdef __DEBUG__
+    else
+        std::cout << "Socket was created successfully fd[" << socket_descriptor << "]\n";
+#endif // __DEBUG__
+}
+
+// can call protocol by name
+Socket::Socket(int domain, int type, const char *protocol_name, int flags)
     : socket_descriptor(invalid_file_descriptor)
 {
-    socket_descriptor = socket(domain, type, protocol);
+    if (!protocol_name)
+        throw std::runtime_error(ERR_NULL_MSG);
+
+    DEBUGASSERT(domain > 0);
+
+    // identify the protocol name
+    std::string proto_name(protocol_name);
+    for (size_t i = 0; i < proto_name.length(); i++)
+        proto_name[i] = std::tolower(proto_name[i]);
+
+    // continue with the rest of the code
+    struct protoent *protocol = getprotobyname(proto_name.c_str());
+    // check getprotobyname return
+    if (!protocol)
+        throw std::runtime_error(ERR_NULL_MSG);
+
+#ifdef __DEBUG__
+    std::cout << "protocol official name is [" << protocol->p_name << "]\n";
+#endif // __DEBUG__
+
+
+    socket_descriptor = socket(domain, type | flags, protocol->p_proto);
+
     // On success, a file descriptor for the new socket is returned.  On error, -1 is
     // returned, and errno is set
     if (socket_descriptor == invalid_file_descriptor)
     {
         std::stringstream errorMessage;
-        errorMessage << SOCKET_ERR << ": " << strerror(errno);
+        errorMessage << ERR_STRT_MSG << ": " << strerror(errno);
         throw std::runtime_error(errorMessage.str());
     }
 #ifdef __DEBUG__ // if needed
     else
-        std::cout << "Listener was created successfully fd[" << listener << "]\n";
+        std::cout << "Socket was created successfully fd[" << socket_descriptor << "]\n";
 #endif // __DEBUG__
 }
 
+// destructor
 Socket::~Socket()
 {
     if (socket_descriptor != invalid_file_descriptor)
@@ -33,20 +108,29 @@ Socket::~Socket()
         close(socket_descriptor);
         socket_descriptor = invalid_file_descriptor; // invalidate the filedescriptor
     }
+#ifdef __DEBUG__
+    else
+        std::cerr << "socket fd[" << socket_descriptor << "] closed!!\n";
+#endif // __DEBUG__
 }
 
 
 // member functions
-
-WS_CODE Socket::bind(int family, int port, int address)
+WS_CODE Socket::bind(int port)
 {
-    // int bind(int sockfd, const struct sockaddr *addr, socklen_t addrlen);
+    // this has to be created based on the type of socket
     struct sockaddr_in addr;
-    addr.sin_addr.s_addr = htonl(address);
+
+    memset(&addr, 0, sizeof(addr));
+    // same for these filled they are determined based on the family type
+    addr.sin_addr.s_addr = htonl(INADDR_ANY);
     addr.sin_family = family;
-    addr.sin_port = port;
-    if (::bind(socket_descriptor, (struct sockaddr *)&addr, sizeof(addr)))
+    addr.sin_port = htons(port);
+
+    int status = ::bind(socket_descriptor, (struct sockaddr *)&addr, sizeof(addr));
+    if (status == -1)
         return WS_BIND_FAIL;
+
     return WS_OK;
 }
 
@@ -54,8 +138,8 @@ WS_CODE Socket::bind(int family, int port, int address)
  * Listens for a connection on a socket and marks the socket referred to by
  * socket_descriptor as a passive socket.
  *
- * @param backlog The maximum length to which the queue of pending connections for sockfd
- * may grow.
+ * @param backlog The maximum length to which the queue of pending connections for
+ * sockfd may grow.
  *
  * @return WS_CODE The status code indicating the result of the listen operation.
  *
@@ -63,8 +147,8 @@ WS_CODE Socket::bind(int family, int port, int address)
  *
  * @note The listen operation is typically used on server sockets after the socket has
  * been bound to a specific address and port using the bind function. Once the listen
- * operation is successful, the server socket can accept incoming connections using the
- * accept function.
+ * operation is successful, the server socket can accept incoming connections using
+ * the accept function.
  */
 WS_CODE Socket::listen(int backlog)
 {
@@ -72,19 +156,6 @@ WS_CODE Socket::listen(int backlog)
     int status = ::listen(socket_descriptor, backlog);
     if (status == -1)
         return WS_LISTEN_FAIL;
+
     return WS_OK;
-}
-
-// deleted but can't cause is 98 maguy
-Socket::Socket(const Socket &other)
-    : socket_descriptor(0)
-{
-    (void)other;
-}
-
-Socket &Socket::operator=(const Socket &other)
-{
-
-    (void)other;
-    return *this;
 }
