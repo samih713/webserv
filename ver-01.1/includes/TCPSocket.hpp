d03a82a04641995fbbf1a85214eb5c93461f1d27
Samih <sakmohamed@gmail.com>
2024-01-06 04:10:37 +0400

    feat(TCPSocket.hpp): Add static constant variables, and set_port()
    - Add famliy and type static constant variable members, to TCPSocket
     these are used to pass to Socket base class constructor, in order to
     create an appropriate type socket.
    - The set_port() functionality could be removed later on, as it doesn't
     adhere to LSP, for now it's used to set a specific port for later bind()
     calls.

--- ver-01.1/includes/TCPSocket.hpp
+++ ver-01.1/includes/TCPSocket.hpp
@@ -1,19 +1,26 @@
 #include <Socket.hpp>
 #include <sys/socket.h>
 
+// is-a inheritance relationship, every TCPSocket is a Socket
 #ifndef TCPSOCKET_HPP
 #define TCPSOCKET_HPP
 
+// a non-blocking SOCK_STREAM, TCP(IP4) socket bound to port
 class TCPSocket : public Socket
 {
 
     public:
         TCPSocket();
+        void set_port(int port) throw();
         ~TCPSocket();
 
     private:
         TCPSocket(const TCPSocket &);
         TCPSocket &operator=(const TCPSocket &);
+
+        // TCPSocket parameters
+        static const int family = AF_INET;
+        static const int type = SOCK_STREAM;
 };
 
 #endif // TCPSOCKET_HPP
