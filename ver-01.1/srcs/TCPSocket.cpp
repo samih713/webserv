c7860642917c6b0f256bc514209b172f36f70d4c
Samih <sakmohamed@gmail.com>
2024-01-06 04:15:04 +0400

    feat(TCPSocket.cpp): Add member function implementations
    - Add default constructor implementations, that preps the sockaddr_in
     struct, with the pre-defined constant variables for the TCPSocket class
    - Add set_port() function implementation

--- ver-01.1/srcs/TCPSocket.cpp
+++ ver-01.1/srcs/TCPSocket.cpp
@@ -1,10 +1,31 @@
 #include "TCPSocket.hpp"
+#include <netinet/in.h>
+#include <sys/socket.h>
 
 TCPSocket::TCPSocket()
-    : Socket(AF_INET, SOCK_STREAM, 0, SOCK_NONBLOCK)
-{}
+    : Socket(family, type, 0, SOCK_NONBLOCK)
+{
+    // zero out the sockaddr struct before copying
+    std::memset(&address, 0, sizeof(address));
+    struct sockaddr_in addr;
+
+    // prep up the struct
+    addr.sin_addr.s_addr = htonl(INADDR_ANY);
+    addr.sin_family = family;
+    std::memcpy(&address, &addr, sizeof(addr));
+}
+
+void TCPSocket::set_port(int port) throw()
+{
+    std::memset(&address, 0, sizeof(address));
+    struct sockaddr_in addr;
+    addr.sin_port = htons(port);
+    std::memcpy(&address, &addr, sizeof(addr));
+}
 
 TCPSocket::~TCPSocket()
 {
-    // destructor code
+#ifdef __DEBUG__
+    std::cerr << "TCPSocket closed!!\n";
+#endif // __DEBUG__
 }
