6c0fb2c0fc74ac0230b8327df4ebf8a7e5516490
samih713 <sakmohamed@gmail.com>
2024-01-15 00:00:34 +0400

    chore(Socket): Add debug macro

--- ver-01.1/includes/Socket.hpp
+++ ver-01.1/includes/Socket.hpp
@@ -21,33 +21,34 @@
 
 /* --------------------------------- colors --------------------------------- */
 #define SET_COLOR(color) (std::cout << (color))
-#define RE_COLOR (std::cout << RE)
+#define RE_COLOR         (std::cout << RE)
 
 #define RE "\001\033[0;39m\002" // reset
-#define L "\001\033[1;92m\002"  // green light
-#define Y "\001\033[1;93m\002"  // yellow
-#define M "\001\033[1;95m\002"  // magenta
-#define R "\001\033[1;91m\002"  // red
-#define B "\001\033[1;94m\002"  // blue
-#define C "\001\033[1;96m\002"  // cyan
-#define W "\001\033[1;97m\002"  // white
-#define D "\001\033[1;30m\002"  // gray
-#define O "\001\033[1;33m\002"  // orange
+#define L  "\001\033[1;92m\002" // green light
+#define Y  "\001\033[1;93m\002" // yellow
+#define M  "\001\033[1;95m\002" // magenta
+#define R  "\001\033[1;91m\002" // red
+#define B  "\001\033[1;94m\002" // blue
+#define C  "\001\033[1;96m\002" // cyan
+#define W  "\001\033[1;97m\002" // white
+#define D  "\001\033[1;30m\002" // gray
+#define O  "\001\033[1;33m\002" // orange
 
 /* ------------------------------- DEBUG MACRO ------------------------------ */
 #ifndef __DEBUG__
 #define DEBUG_MSG(message, color)
 #else
-#define DEBUG_MSG(message, color)                                                   \
-  do                                                                                \
-    {                                                                               \
-      std::cerr << (color);                                                         \
-      std::cerr << (message);                                                       \
-      std::cerr << std::endl;                                                       \
-      std::cerr << RE;                                                              \
-      std::cerr.flush ();                                                           \
-    }                                                                               \
-  while (0)
+#include <cassert>
+#include <iostream>
+#define DEBUG_MSG(message, color)                                                        \
+    do                                                                                   \
+    {                                                                                    \
+        std::cerr << (color);                                                            \
+        std::cerr << message;                                                            \
+        std::cerr << std::endl;                                                          \
+        std::cerr << RE;                                                                 \
+        std::cerr.flush();                                                               \
+    } while (0)
 #endif // __DEBUG__
 
 
@@ -64,8 +65,6 @@ static const int SOCK_FLAG = 0;
 
 // DEBUG BUILD
 #ifdef __DEBUG__
-#include <cassert>
-#include <iostream>
 #define DEBUGASSERT(x) assert(x)
 #else
 #define DEBUGASSERT(x)                                                                   \
