3eaf1e96e5dff9ccd4b0dadf00fe02f0dd909fbd
Tanvir Ahmed Anas <tanvir.program@gmail.com>
2024-01-17 14:38:24 +0400

    chore(Socket.hpp, error.hpp):
    - Moved error/debug related macros, functions and variables into
    error.hpp

--- ver-01.1/includes/Socket.hpp
+++ ver-01.1/includes/Socket.hpp
@@ -7,6 +7,7 @@
 #include <string>
 #include <sys/socket.h>
 #include <unistd.h>
+#include "error.hpp"
 
 // TODO // [P]artially implemented, needs [I]mprovement, [X] done
 //
@@ -14,43 +15,9 @@
 // [ ] add shutdown functionality (block recv, block send, close())
 // [ ] fix the get_fd functionality is it really necessary
 // [ ] split-up function implementations into their own files
-// [ ] make a seperate error header
 
 #ifndef SOCKET_HPP
 #define SOCKET_HPP
-
-/* --------------------------------- colors --------------------------------- */
-#define SET_COLOR(color) (std::cout << (color))
-#define RE_COLOR         (std::cout << RE)
-
-#define RE "\001\033[0;39m\002" // reset
-#define L  "\001\033[1;92m\002" // green light
-#define Y  "\001\033[1;93m\002" // yellow
-#define M  "\001\033[1;95m\002" // magenta
-#define R  "\001\033[1;91m\002" // red
-#define B  "\001\033[1;94m\002" // blue
-#define C  "\001\033[1;96m\002" // cyan
-#define W  "\001\033[1;97m\002" // white
-#define D  "\001\033[1;30m\002" // gray
-#define O  "\001\033[1;33m\002" // orange
-
-/* ------------------------------- DEBUG MACRO ------------------------------ */
-#ifndef __DEBUG__
-#define DEBUG_MSG(message, color)
-#else
-#include <cassert>
-#include <iostream>
-#define DEBUG_MSG(message, color)                                                        \
-    do                                                                                   \
-    {                                                                                    \
-        std::cerr << (color);                                                            \
-        std::cerr << message;                                                            \
-        std::cerr << std::endl;                                                          \
-        std::cerr << RE;                                                                 \
-        std::cerr.flush();                                                               \
-    } while (0)
-#endif // __DEBUG__
-
 
 // SOCK_FLAG
 // prefer the compiler over pre-processor
@@ -63,33 +30,11 @@ static const int SOCK_FLAG = O_NONBLOCK;
 static const int SOCK_FLAG = 0;
 #endif
 
-// DEBUG BUILD
-#ifdef __DEBUG__
-#define DEBUGASSERT(x) assert(x)
-#else
-#define DEBUGASSERT(x)                                                                   \
-    do                                                                                   \
-    {                                                                                    \
-    } while (0)
-#endif // __DEBUG__
-// DEBUG BUILD
-
-// [ERROR MESSAGES]
-static std::string ERR_NULL("Socket: null ptr error");
-static std::string ERR_CREAT("Socket: creation failed");
-static std::string ERR_BIND("Socket: binding failed");
-static std::string ERR_NBIND("Socket: not bound to a port");
-static std::string ERR_LIST("Socket: listen failed");
-static std::string ERR_NLIST("Socket: not listeing");
-static std::string ERR_ACCP("Socket: accept failed");
-
 // socket_descriptor type
 typedef int file_descriptor;
 
-
 class Socket
 {
-
     protected: // protected constructors only in derived classes
         /*  [CONSTRUCTORS] */
 
@@ -139,8 +84,6 @@ class Socket
                     return error_message.c_str();
                 }
         };
-        ;
-
 
         file_descriptor socket_descriptor;
 
@@ -152,11 +95,8 @@ class Socket
         mutable bool is_listening;
 
         // deleted but can't cause is 98 maguy
-        Socket(const Socket &){};
-        Socket &operator=(const Socket &)
-        {
-            return *this;
-        };
+        Socket(const Socket &) {};
+        Socket &operator=(const Socket &) { return *this; };
 };
 
 #endif // SOCKET_HPP
