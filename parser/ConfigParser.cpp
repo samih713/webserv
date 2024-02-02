732c65621cbc7d563e0b8c5be296bd9dbf31c4c0
Taanviir <66136914+Taanviir@users.noreply.github.com>
2024-02-02 16:54:40 +0400

    feat(ConfigParser):
    - Implemented JsonParser(), parseJSON()
    - Added definitions for parsing functions
    - Implemented parseObject(), parseArray()

--- parser/ConfigParser.cpp
+++ parser/ConfigParser.cpp
@@ -1,28 +1,6 @@
 #include "ConfigParser.hpp"
 
-using namespace ConfigParser;
-
-JsonValue ConfigParser::parseJSON(const std::string file) {
-    std::string json;
-    readFile(file, json);
-
-    stringIterator itr = json.begin();
-    if (*itr != '{')
-        throw std::runtime_error(ERR_JSON_PARSE);
-    itr++;
-    return extractJSON(json, itr);
-}
-
-/**
- * @brief Read the JSON file, remove all ( \t\n\r) characters from each line
- * and store the content in a string
- * 
- * @param filepath
- * @param content string to store the content of the file
- * 
- * @throw std::runtime_error if the file cannot be opened
-*/
-void ConfigParser::readFile(const std::string& filepath, std::string& content) {
+JsonParser::JsonParser(const std::string filepath) {
     std::cout << "Reading config file: " << filepath << std::endl;
 
     std::ifstream inputFileStream(filepath.c_str());
@@ -34,100 +12,80 @@ void ConfigParser::readFile(const std::string& filepath, std::string& content) {
         line.erase(remove_if(line.begin(), line.end(), isspace), line.end());
         if (line.empty())
             continue;
-        content.append(line);
+        _content.append(line);
     }
     inputFileStream.close();
+    _itr = _content.begin();
 }
 
-JsonValue ConfigParser::extractJSON(const std::string& content, stringIterator& itr) {
-    std::map<std::string, JsonValue>* _jsonMap = new std::map<std::string, JsonValue>;
-    if (_jsonMap == NULL)
-        throw std::runtime_error(ERR_MEMORY_ALLOC);
-
-    do {
-        KeyValuePair pair = parseKeyValuePair(content, itr);
-        _jsonMap->insert(pair);
-    } while (*itr == ',' && ++itr != content.end());
-
-    return (JsonValue){ .jsonMap = _jsonMap };
+JsonValue JsonParser::parseJSON(void) {
+    _itr2 = _content.begin();
+    std::cout << "Parsing JSON" << std::endl;
+    return parseValue();
 }
 
-KeyValuePair ConfigParser::parseKeyValuePair(const std::string& content, stringIterator& itr) {
-    if (itr == content.end())
+JsonValue JsonParser::parseValue(void) {
+    if (_itr == _content.end())
         throw std::runtime_error(ERR_JSON_PARSE);
 
-    // parsing key
-    std::string key;
-    stringIterator tempItr;
-    if (*itr == '\"') {
-        tempItr = ++itr;
-        while (*itr != '\"')
-            ++itr;
-        key = std::string(tempItr, itr);
-    }
+}
 
-        if (*(++itr) != ':')
-            throw std::runtime_error(ERR_JSON_PARSE);
-        itr++;
+JsonValue JsonParser::parseNull(void) {
+}
 
-    JsonValue value; // value can be string, number(int), boolean, null, array or object
-    if ((*itr) == '\"' && isalpha(*(++itr))) {
-        tempItr = itr;
-        while (*itr != '\"')
-            ++itr;
-        value.string = new std::string(tempItr, itr);
-        if (*(++itr) != ',' && *itr != '}')
-            throw std::runtime_error(ERR_JSON_PARSE);
-    }
-    else if (isdigit(*itr) || (*itr == '-' && isdigit(*(++itr)))) {
-        tempItr = itr;
-        while (isdigit(*itr)) {
-            if (*itr == '.')
-                throw std::runtime_error(ERR_JSON_TYPE);
-            ++itr;
-        }
-        value.number = std::atoi(std::string(tempItr, itr).c_str());
-        if (*itr != ',' && *itr != '}' && *itr != ']')
-            throw std::runtime_error(ERR_JSON_PARSE);
-    }
-    else if (std::string(itr, itr + 4) == "null") {
-        value.string = NULL;
-        itr += 4;
-        if (*itr != ',' && *itr != '}')
-            throw std::runtime_error(ERR_JSON_PARSE);
-    }
-    else if (std::string(itr, itr + 5) == "false") {
-        value.boolean = false;
-        itr += 5;
-        if (*itr != ',' && *itr != '}')
-            throw std::runtime_error(ERR_JSON_PARSE);
-    }
-    else if (std::string(itr, itr + 4) == "true") {
-        value.boolean = true;
-        itr += 4;
-        if (*itr != ',' && *itr != '}')
-            throw std::runtime_error(ERR_JSON_PARSE);
-    }
-    else if ((*itr) == '[') {
-        value.array = new std::vector<JsonValue>;
-        itr++;
-        while (itr != content.end() && *itr != ']') {
-            value.array->push_back(extractJSON(content, itr));
-            if (*itr != ',' && *itr != ']')
-                throw std::runtime_error(ERR_JSON_PARSE);
-            if (*itr == ',')
-                itr++;
-        }
-        if (*itr != ']' && (*(++itr) != ',' || *itr != '}'))
-            throw std::runtime_error(ERR_JSON_PARSE);
-        itr++;
-    }
-    else if (*itr == '{') {
-        value = extractJSON(content, ++itr);
-        if (*itr != ',' && *itr != '}')
-            throw std::runtime_error(ERR_JSON_PARSE);
-    }
-    else
+JsonValue JsonParser::parseString(void) {
+}
+
+JsonValue JsonParser::parseNumber(void) {
+}
+
+JsonValue JsonParser::parseBoolean(void) {
+    // _itr moved from 't' or 'f' to after 'e'
+}
+
+JsonValue JsonParser::parseArray(void) {
+    // _itr moved from '[' to after ']'
+    // ',' or '}' should be right after
+    _itr++; // move from '['
+    if (*_itr != '\"' && !isdigit(*_itr) && *_itr != 'f' && *_itr != 't' && *_itr != 'n' && *_itr != '[' && *_itr != '{')
         throw std::runtime_error(ERR_JSON_PARSE);
-    return std::make_pair(key, value);
+    std::vector<JsonValue> array;
+    while (_itr != _content.end() && *_itr != ']') {
+        array.push_back(parseValue());
+        if (*_itr != ',' && *_itr != ']')
+            throw std::runtime_error(ERR_JSON_PARSE);
+        if (*_itr == ',')
+            _itr++;
+    }
+    if (*_itr != ']' && (*(++_itr) != ',' || *_itr != '}'))
+        throw std::runtime_error(ERR_JSON_PARSE);
+    _itr++;
+    JsonValue result;
+    result.setArray(array);
+    return result;
+}
+
+JsonValue JsonParser::parseObject(void) {
+    // _itr moved from '{' to after '}'
+    // ',' or '}' or EOF should be right after
+    _itr++; // move from '{'
+    std::map<std::string, JsonValue> value;
+    while (_itr != _content.end() && *_itr != '}') {
+        if (*_itr != '\"')
+            throw std::runtime_error(ERR_JSON_PARSE);
+        std::string key = parseString().asString();
+        _itr++; // move from ':'
+        value[key] = parseValue();
+        if (*_itr != ',' && *_itr != '}')
+            throw std::runtime_error(ERR_JSON_PARSE);
+        if (*_itr == ',')
+            _itr++;
+    }
+    if (*(++_itr) != ',' && *_itr != ']' && _itr != _content.end()) {
+        std::cout << *_itr << std::endl;
+        throw std::runtime_error(ERR_JSON_PARSE);
+    }
+    JsonValue result;
+    result.setObject(value);
+    return result;
 }
