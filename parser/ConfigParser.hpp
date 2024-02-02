e4a4d70b9345e5a734c74fdc7bcc840209972d6b
Taanviir <66136914+Taanviir@users.noreply.github.com>
2024-02-02 16:41:06 +0400

    refactor(ConfigParser.hpp):
    - Removed union implementation to reduce code complexity
    - Implemented classes JsonValue & JsonParser
    - JsonValue contains definitions for str, int, vector, map types
        - Implemented getters and setters as well
        - Implemented enum for variable types
    - JsonParser contains definitions for parsing functions
        - Also contains content and iterator as private variables

--- parser/ConfigParser.hpp
+++ parser/ConfigParser.hpp
@@ -6,23 +6,61 @@
 #include <map>
 #include <cstring>
 
-namespace ConfigParser {
-    union JsonValue {
-        int number;
-        bool boolean;
-        std::string* string; // pointer to string to avoid copying
-        std::vector<JsonValue>* array;
-        std::map<std::string, JsonValue>* jsonMap;
+class JsonValue {
+public:
+    enum Type { JSON_NULL, JSON_BOOL, JSON_NUMBER, JSON_STRING, JSON_ARRAY, JSON_OBJECT };
+
+    JsonValue(): _type(JSON_NULL) {}
+    ~JsonValue() {};
+
+    void setNull() { _type = JSON_NULL; };
+    void setBoolean(bool boolean) { _type = JSON_BOOL; _boolean = boolean; };
+    void setNumber(int number) { _type = JSON_NUMBER; _number = number; };
+    void setString(const std::string& string) { _type = JSON_STRING; _string = string; };
+    void setArray(const std::vector<JsonValue>& array) { _type = JSON_ARRAY; _array = array; };
+    void setObject(const std::map<std::string, JsonValue>& object) { _type = JSON_OBJECT; _object = object; };
+
+    Type getType() const { return _type; };
+
+    bool asBoolean() const { return _boolean; };
+    int asNumber() const { return _number; };
+    const std::string& asString() const { return _string; };
+    const std::vector<JsonValue>& asArray() const { return _array; };
+    const std::map<std::string, JsonValue>& asObject() const { return _object; };
+
+private:
+    Type _type;
+    union {
+        int _number;
+        bool _boolean;
     };
+    std::string _string;
+    std::vector<JsonValue> _array;
+    std::map<std::string, JsonValue> _object;
+};
 
-    typedef std::pair<std::string, JsonValue> KeyValuePair;
-    typedef std::string::iterator stringIterator;
+typedef std::string::iterator stringIterator;
 
-    JsonValue parseJSON(const std::string file);
-    void readFile(const std::string& filepath, std::string& content);
-    KeyValuePair parseKeyValuePair(const std::string& content, stringIterator& itr);
-    JsonValue extractJSON(const std::string& json, stringIterator& itr);
-    void clearJSON(JsonValue& parsed);
+class JsonParser {
+public:
+    JsonParser(const std::string filepath);
+    ~JsonParser() {};
+
+    JsonValue parseJSON(void);
+    void printJSON(JsonValue json);
+
+private:
+    std::string _content;
+    stringIterator _itr;
+    stringIterator _itr2;
+
+    JsonValue parseValue(void);
+    JsonValue parseNull(void);
+    JsonValue parseString(void);
+    JsonValue parseNumber(void);
+    JsonValue parseBoolean(void);
+    JsonValue parseArray(void);
+    JsonValue parseObject(void);
 };
 
 #endif // CONFIG_PARSER_HPP
