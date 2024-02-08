7d1b24463263352299d705b434ebca9ddafa2ba8
Samih <sakmohamed@gmail.com>
2024-02-08 16:22:00 +0400

    chore(webserv.hpp): Fix spelling mistak

--- includes/webserv.hpp
+++ includes/webserv.hpp
@@ -5,12 +5,12 @@
 #define to_str(name) #name
 
 /* -------------------------------- INCLUDES -------------------------------- */
+#include "debug.hpp"
+#include <algorithm>
+#include <cstdlib>
 #include <iostream>
 #include <string>
-#include <cstdlib>
-#include <algorithm>
 #include <vector>
-#include "debug.hpp"
 
 /* ----------------------------- ERROR MESSAGES ----------------------------- */
 static std::string ERR_NULL("Socket: null ptr error");
@@ -18,7 +18,7 @@ static std::string ERR_CREAT("Socket: creation failed");
 static std::string ERR_BIND("Socket: binding failed");
 static std::string ERR_NBIND("Socket: not bound to a port");
 static std::string ERR_LIST("Socket: listen failed");
-static std::string ERR_NLIST("Socket: not listeing");
+static std::string ERR_NLIST("Socket: not listening");
 static std::string ERR_ACCP("Socket: accept failed");
 
 static std::string ERR_OPEN("Parser: cannot open file");
