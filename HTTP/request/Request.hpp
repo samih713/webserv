085a4892a87dfc13518f0e6edc06208d94932498
Samih <sakmohamed@gmail.com>
2024-02-19 21:22:49 +0400

    chore(Request.hpp): Add shorter name for vector of headers

--- HTTP/request/Request.hpp
+++ HTTP/request/Request.hpp
@@ -2,18 +2,7 @@
 #define REQUEST_HPP
 
 #include "Message.hpp"
-#include <map>
-#include <ostream>
-#include <string>
-#include <vector>
-
-using std::istream;
-using std::map;
-using std::ostream;
-using std::pair;
-using std::string;
-using std::stringstream;
-using std::vector;
+#include "webserv.hpp"
 
 namespace webserv
 {
@@ -27,20 +16,21 @@ class Request
         Request(const string &rawRequest);
         ~Request();
         Request(const Request &other);
-        void            parse();
-        friend ostream &operator<<(ostream &os, const Request &r);
         METHOD          get_method() const;
+        const string   &get_resource() const;
+        const vps      &get_headers() const;
+        friend ostream &operator<<(ostream &os, const Request &r);
 
     private:
+        void         parse();
         const string rawRequest;
         // request-line   = method SP request-target SP HTTP-version
         METHOD method;
         string resource;
         string http_version;
-        // clang-format off
         // field-line   = field-name ":" OWS field-value OWS
-        vector<pair<string, string> > header_fields;
-        vector<pair<string, string> > trailer_fields;
+        vps header_fields;
+        vps trailer_fields;
         // clang-format on
         std::string body;
         // deleted copy assigment
