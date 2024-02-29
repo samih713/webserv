bbc3270feb01c8f93caf795bae73dd739501261c
hashimMH <hashimstar25@gmail.com>
2024-02-29 18:02:59 +0400

    test(CGI main): remembering execve

--- CGI/tester/main.cpp
+++ CGI/tester/main.cpp
@@ -1,15 +1,36 @@
 
 #include <iostream>
-#include <stdexcept>
+# include <stdio.h>
+# include <unistd.h>
+# include <fcntl.h>
+# include <signal.h>
+# include <sys/stat.h>
+# include <sys/wait.h>
+# include <stdexcept>
 
 
-using namespace webserv::http;
+//using namespace webserv::http;
 
 
-
-int main()
+int	main(int ac, char **av, char **env)
 {
+	(void)ac;
+	//(void)av;
+	int		fd[2];
+	int		id;
 
+	pipe(fd);
+	id = fork();
+	if (id == 0)
+	{
+		//dup2(fd[0], STDIN_FILENO);
+		close(fd[0]);
+		close(fd[1]);
+		if (execve("/bin/ls", ++av, env))
+			write(2, "error\n", 6);
+	}
+	close(fd[0]);
+	close(fd[1]);
 
-    return 0;
+	return (0);
 }
