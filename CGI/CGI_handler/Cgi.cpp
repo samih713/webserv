832769f6c5a3859cb875427767bbcaa4a79f19f9
hashimMH <hashimstar25@gmail.com>
2024-03-04 12:31:42 +0400

    build(CGI):implement ecxute fun for cgi

--- CGI/CGI_handler/Cgi.cpp
+++ CGI/CGI_handler/Cgi.cpp
@@ -6,9 +6,46 @@
 /*   By: hmohamed <hmohamed@student.42abudhabi.ae>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/23 12:44:51 by hmohamed          #+#    #+#             */
-/*   Updated: 2024/02/23 12:45:09 by hmohamed         ###   ########.fr       */
+/*   Updated: 2024/03/04 12:27:05 by hmohamed         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include  "Cgi.hpp"
 
+
+Cgi::Cgi(char* filePath, char** arguments, char** environment): filePath(filePath), arguments(arguments), environment(environment)
+{
+	
+}
+
+Cgi::~Cgi()
+{
+
+}
+
+void Cgi::execute()
+{
+    int fd[2];
+    int id;
+
+    pipe(fd);
+    id = fork();
+    if (id == 0)
+    {
+        // Child process
+        close(fd[0]);
+        close(fd[1]);
+
+        if (execve(filePath, arguments, environment) == -1)
+        {
+            std::cerr << "Error executing execve: " << strerror(errno) << std::endl;
+            _exit(EXIT_FAILURE);
+        }
+    }
+    close(fd[0]);
+    close(fd[1]);
+
+    // Wait for the child process to finish
+    int status;
+    waitpid(id, &status, 0);
+}
--- CGI/CGI_handler/Cgi.hpp
+++ CGI/CGI_handler/Cgi.hpp
@@ -1,25 +1,38 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   CGI.hpp                                            :+:      :+:    :+:   */
+/*   Cgi.hpp                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: hmohamed <hmohamed@student.42abudhabi.ae>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/23 12:42:45 by hmohamed          #+#    #+#             */
-/*   Updated: 2024/02/23 12:44:28 by hmohamed         ###   ########.fr       */
+/*   Updated: 2024/03/04 12:26:54 by hmohamed         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef CGI_HPP
 #define CGI_HPP
 
-#include "webserv.hpp"
+
+#include <iostream>
+#include <cstring>
+#include <string>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
 
 class Cgi {
-	private:
-
 	public:
+    Cgi(char* filePath, char** arguments, char** environment);
+	~Cgi();
+    void execute();
+
+private:
+    char* filePath;
+    char** arguments;
+    char** environment;
 	
 };
 
 #endif
+
