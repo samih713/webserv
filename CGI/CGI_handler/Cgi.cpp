fb222549b66e192aca3d025e9b3d3327f0d686e7
hashimMH <hashimstar25@gmail.com>
2024-03-06 16:47:20 +0400

    build(cgi.cpp):save the res into string

--- CGI/CGI_handler/Cgi.cpp
+++ CGI/CGI_handler/Cgi.cpp
@@ -6,7 +6,7 @@
 /*   By: hmohamed <hmohamed@student.42abudhabi.ae>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/23 12:44:51 by hmohamed          #+#    #+#             */
-/*   Updated: 2024/03/04 12:27:05 by hmohamed         ###   ########.fr       */
+/*   Updated: 2024/03/06 15:24:45 by hmohamed         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,12 +27,17 @@ void Cgi::execute()
 {
     int fd[2];
     int id;
+	char *res_body;
+	int a;
 
+	res_body = (char *)malloc(100);
+	res_body[99] = '\0';
     pipe(fd);
     id = fork();
     if (id == 0)
     {
         // Child process
+		dup2(fd[1], STDOUT_FILENO);
         close(fd[0]);
         close(fd[1]);
 
@@ -42,10 +47,15 @@ void Cgi::execute()
             _exit(EXIT_FAILURE);
         }
     }
+	
+	// Wait for the child process to finish
+    int status;
+    waitpid(id, &status, 0);
+	a = read(fd[0], res_body, 90);
+	res_body[a] = '\0';
+	cout << res_body << endl;
     close(fd[0]);
     close(fd[1]);
 
-    // Wait for the child process to finish
-    int status;
-    waitpid(id, &status, 0);
+    
 }
--- CGI/CGI_handler/Cgi.hpp
+++ CGI/CGI_handler/Cgi.hpp
@@ -6,25 +6,27 @@
 /*   By: hmohamed <hmohamed@student.42abudhabi.ae>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/23 12:42:45 by hmohamed          #+#    #+#             */
-/*   Updated: 2024/03/04 12:26:54 by hmohamed         ###   ########.fr       */
+/*   Updated: 2024/03/06 15:30:14 by hmohamed         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef CGI_HPP
 #define CGI_HPP
 
-
+#include "../../includes/webserv.hpp"
 #include <iostream>
 #include <cstring>
 #include <string>
 #include <unistd.h>
 #include <sys/types.h>
 #include <sys/wait.h>
+#include "Request.hpp"
 
 class Cgi {
 	public:
     Cgi(char* filePath, char** arguments, char** environment);
 	~Cgi();
+	void handler(Request req);
     void execute();
 
 private:
