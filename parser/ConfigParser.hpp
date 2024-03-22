e79b57a485f3ccea5b5551a5b2567c28a7102573
Taanviir <66136914+Taanviir@users.noreply.github.com>
2024-03-22 04:37:23 +0400

    chore(ConfigParser.hpp): Moved error messages from webserv.hpp

--- parser/ConfigParser.hpp
+++ parser/ConfigParser.hpp
@@ -6,6 +6,14 @@
 #include <sys/stat.h>
 #include <stack>
 
+static std::string ERR_INVALID_FILE("Parser: invalid file");
+static std::string ERR_STAT("Parser: not a regular file");
+static std::string ERR_OPEN("Parser: cannot open file");
+static std::string ERR_EMPTY("Parser: file is empty");
+static std::string ERR_CLOSING_BRACE("Parser: } missing");
+static std::string ERR_OPENINING_BRACE("Parser: { missing");
+static std::string ERR_MISSING_SEMICOLON("Parser: semicolon missing");
+
 class ConfigParser {
 public:
     ConfigParser(std::string const& filepath);
