774f829f09a6008d858edfde206a831c1c3b5b4c
Tanvir Ahmed Anas <tanvir.program@gmail.com>
2024-04-02 23:37:53 +0400

    feat(parser): Implemented server and http
    - server is a WIP

--- parser/ConfigParser.cpp
+++ parser/ConfigParser.cpp
@@ -63,43 +63,103 @@ void ConfigParser::_validateBraces(void) {
         throw runtime_error(ERR_CLOSING_BRACE);
 }
 
+void ConfigParser::_parseIndexDirective(void) {
+    DEBUG_MSG("Parsing index directive", RE);
+}
+
+void ConfigParser::_parseLocationContext(void) {
+    DEBUG_MSG("Parsing location context", RE);
+}
+
 void ConfigParser::_parseServerContext(void) {
+    DEBUG_MSG("Parsing server context", RE);
+
     ++_itr; // move to {
     if (*_itr != "{")
         throw runtime_error(ERR_OPENINING_BRACE);
+    ++_itr; // move to first directive
 
-    while (*_itr != "}") {
-        std::cout << *_itr << std::endl;
+    while (*_itr != "}")
+    {
         if (*_itr == "listen") {
+            //! listen can also handle IP address like 0.0.0.0:80
             ++_itr; // move to port number
-            for (string::const_iterator portItr = _itr->begin(); portItr != _itr->end(); ++portItr) {
-                if (!std::isdigit(*portItr))
-                    throw runtime_error(ERR_INVALID_LISTEN);
-            }
+            if (*(_itr + 1) != ";")
+                throw runtime_error(ERR_MISSING_SEMICOLON);
+
+            if (!_isStringNumber(*_itr))
+                throw runtime_error(ERR_INVALID_LISTEN);
+
             _config.listenerPort = std::atoi(_itr->c_str());
             if (_config.listenerPort > MAX_PORT || _config.listenerPort < 0)
                 throw runtime_error(ERR_INVALID_LISTEN);
-        }
-        else if (*_itr == "server_name") {
+            ++_itr; // move to semicolon
+        } else if (*_itr == "server_name") {
             ++_itr; // move to server name
             while (*_itr != ";") {
+                if (_isKeyword(*_itr))
+                    throw runtime_error(ERR_INVALID_SERVER_NAME);
                 _config.serverName.push_back(*_itr);
                 ++_itr;
             }
+        } else if (*_itr == "error_page") {
+            ++_itr; // move to error code
+            while (_isStringNumber(*_itr)) {
+                // _config.errorPages.codes.push_back(std::atoi(_itr->c_str()));
+                ++_itr; // move to next error code
+            }
+            if (*_itr == ";")
+                throw runtime_error(ERR_ERROR_PATH);
+
+            // _config.errorPages.page = *_itr;
+            _checkSemicolon();
+        } else if (*_itr == "root") {
+            ++_itr; // move to root path
+            _config.serverRoot = *_itr;
+            _checkSemicolon();
+        } else if (*_itr == "index") {
+            _parseIndexDirective();
+        } else if (*_itr == "location") {
+            _parseLocationContext();
+        } else {
+            cout << "ERROR: " << *_itr << endl;
+            throw runtime_error(ERR_UNEXPECTED_TOKENS_IN_SERVER);
         }
-        else if (*_itr == "location") {
-            // ++_itr; // move to location path
-            // _config.locations.push_back(*_itr);
-            while (*_itr != "}")
-                ++_itr;
+        if (*_itr == ";")
             ++_itr;
-        }
-        ++_itr;
     }
 }
 
+void ConfigParser::_parseHTTPContext(void) {
+    DEBUG_MSG("Parsing HTTP context", RE);
+
+    // setting values for Config object
+    _itr = _tokens.begin();
+    if (*_itr != "http") //! maybe there's stuff in the global context
+        throw runtime_error(ERR_MISSING_HTTP);
+
+    ++_itr; // move to {
+    if (*_itr != "{")
+        throw runtime_error(ERR_OPENINING_BRACE);
+    ++_itr; // move to first directive
+
+    while (_itr != _tokens.end()) {
+        if (*_itr == "}")
+            break;
+        if (*_itr == "server")
+            _parseServerContext();
+        //! else if (*_itr == "index")
+        else
+            throw runtime_error(ERR_UNEXPECTED_TOKENS_IN);
+        ++_itr;
+    }
+
+    if ((_itr + 1) != _tokens.end())
+        throw runtime_error(ERR_UNEXPECTED_TOKENS_OUT);
+}
+
 Config ConfigParser::parse(void) {
-    // tokenizing content
+    DEBUG_MSG("Tokenizing content", RE);
     string currentToken;
     for (string::const_iterator itr = _content.begin(); itr != _content.end(); ++itr) {
         if (*itr == '{' || *itr == '}' || *itr == ';') { // delimiters
@@ -120,22 +180,7 @@ Config ConfigParser::parse(void) {
     }
 
     _validateBraces();
-
-    // setting values for Config object
-    _itr = _tokens.begin();
-    if (*_itr != "http")
-        throw runtime_error(ERR_MISSING_HTTP);
-    ++_itr; // move to {
-    if (*_itr != "{")
-        throw runtime_error(ERR_OPENINING_BRACE);
-
-    while (_itr != _tokens.end()) {
-        if (*_itr == "{")
-            ++_itr;
-        if (*_itr == "server")
-            _parseServerContext();
-        ++_itr;
-    }
+    _parseHTTPContext();
 
     return _config;
 }
