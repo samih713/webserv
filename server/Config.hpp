c74d93277428fe0d0d7e2320c074f1aa46ea55ca
Tanvir Ahmed Anas <tanvir.program@gmail.com>
2024-04-03 03:18:25 +0400

    feat(Config): Removed config class and implemented ServerConfig struct along with Location and ErrorPage structs

--- server/Config.hpp
+++ server/Config.hpp
@@ -6,32 +6,72 @@
 
 using webserv::http::STATUS_CODE;
 
-class Config {
-public:
-    Config(string serverRoot = "../resources/sample_pages",
-           string home = "/index.html",
-           string notFoundPage = "/404.html",
-           fd     listenerPort = 8080)
-        : serverRoot(serverRoot)
-        , home(serverRoot + home)
-        , notFoundPage(serverRoot + notFoundPage)
-        , listenerPort(listenerPort)
-    {}
+struct Location {
+    string path;
+    string modifier;
+    string root;
+    vector<string> indexFiles;
+    bool autoindex;
+    Location() : autoindex(false) {}
+};
 
-    string serverRoot;
-    string home;
-    string notFoundPage;
-    fd     listenerPort;
+struct ErrorPage {
+    STATUS_CODE code;
+    string page;
+};
+
+struct ServerConfig {
+    fd listenerPort;
+    string maxBodySize;
     vector<string> serverName;
-    vector<string> locations;
+    string serverRoot;
+    vector<string> indexFiles;
+    vector<Location> locations;
+    vector<ErrorPage> errorPages;
+    bool autoindex;
 
-    void print(void) {
-        std::cout << "Server root: " << serverRoot << std::endl;
-        std::cout << "Home: " << home << std::endl;
-        std::cout << "Not found page: " << notFoundPage << std::endl;
-        std::cout << "Listener port: " << listenerPort << std::endl;
+    ServerConfig() : listenerPort(8080), autoindex(false) {}
+
+    void print(void) const {
+        cout << "ServerConfig {" << endl;
+        cout << "  listenerPort: " << listenerPort << endl;
+        cout << "  maxBodySize: " << maxBodySize << endl;
+        cout << "  autoindex: " << autoindex << endl;
+        cout << "  serverName: [";
+        for (vector<string>::const_iterator itr = serverName.begin(); itr != serverName.end(); ++itr) {
+            cout << *itr;
+            if (itr + 1 != serverName.end())
+                cout << ", ";
+        }
+        cout << "]" << endl;
+        cout << "  serverRoot: " << serverRoot << endl;
+        cout << "  locations: [" << endl;
+        for (vector<Location>::const_iterator itr = locations.begin(); itr != locations.end(); ++itr) {
+            cout << "    Location {" << endl;
+            cout << "      path: " << itr->path << endl;
+            cout << "      modifier: " << itr->modifier << endl;
+            cout << "      root: " << itr->root << endl;
+            cout << "      indexFiles: [";
+            for (vector<string>::const_iterator itr2 = itr->indexFiles.begin(); itr2 != itr->indexFiles.end(); ++itr2) {
+                cout << *itr2;
+                if (itr2 + 1 != itr->indexFiles.end())
+                    cout << ", ";
+            }
+            cout << "]" << endl;
+            cout << "      autoindex: " << itr->autoindex << endl;
+            cout << "    }" << endl;
+        }
+        cout << "  ]" << endl;
+        cout << "  errorPages: [" << endl;
+        for (vector<ErrorPage>::const_iterator itr = errorPages.begin(); itr != errorPages.end(); ++itr) {
+            cout << "    ErrorPage {" << endl;
+            cout << "      code: " << itr->code << endl;
+            cout << "      page: " << itr->page << endl;
+            cout << "    }" << endl;
+        }
+        cout << "  ]" << endl;
+        cout << "}" << endl;
     }
-
 };
 
 #endif // CONFIG_HPP
