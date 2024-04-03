9c5e5527ebd60b788f9260ccf5dcb5a4a2aaee2b
Taanviir <66136914+Taanviir@users.noreply.github.com>
2024-04-03 19:55:15 +0400

    feat: set up main for testing, still WIP

--- main.cpp
+++ main.cpp
@@ -1,3 +1,26 @@
-int main(void) {
-	return 0;
+#include "server/Server.hpp"
+#include "parser/ConfigParser.hpp"
+#include "server/Config.hpp"
+#include "includes/webserv.hpp"
+
+int main()
+{
+    // default constructor of config
+    // Config(string serverRoot = "../resources/sample_pages/",
+    // string home = "index.html",
+    // string notFoundPage = "404.html",
+    // fd     listenerPort = 8080)
+    //
+    try
+    {
+        ConfigParser parser("configs/webserv.conf");
+        vector<ServerConfig> configs = parser.parse();
+        // configs[0].print();
+        Server &webserv = Server::get_instance(configs[0], 10);
+        webserv.start(SELECT);
+    }
+    catch (std::exception &e)
+    {
+        std::cerr << e.what();
+    }
 }
