f74ae5effcb3f593d6eee29566757eff9f12275b
Taanviir <66136914+Taanviir@users.noreply.github.com>
2024-04-03 22:26:22 +0400

    feat(main): added argv check and use

--- main.cpp
+++ main.cpp
@@ -3,8 +3,11 @@
 #include "server/Config.hpp"
 #include "includes/webserv.hpp"
 
-int main()
-{
+int main(int argc, char **argv) {
+    if (argc != 2) {
+        std::cerr << "Usage: " << argv[0] << " <config_file>" << std::endl;
+        exit(1);
+    }
     // default constructor of config
     // Config(string serverRoot = "../resources/sample_pages/",
     // string home = "index.html",
@@ -13,7 +16,7 @@ int main()
     //
     try
     {
-        ConfigParser parser("configs/webserv.conf");
+        ConfigParser parser(argv[1]);
         vector<ServerConfig> configs = parser.parse();
         // configs[0].print();
         Server &webserv = Server::get_instance(configs[0], 10);
