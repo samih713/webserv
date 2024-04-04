4d793179e969730fcbe2cbb288e0a6b407ee5397
Tanvir Ahmed Anas <tanvir.program@gmail.com>
2024-04-05 01:44:09 +0400

    feat(main.cpp): Added default.conf

--- sources/main.cpp
+++ sources/main.cpp
@@ -4,13 +4,17 @@
 #include "../includes/webserv.hpp"
 
 int main(int argc, char **argv) {
-    if (argc != 2) {
-        std::cerr << "Usage: " << argv[0] << " <config_file>" << std::endl;
+    if (argc > 2) {
+        std::cerr << "Usage 1: " << argv[0] << std::endl;
+        std::cerr << "Usage 2: " << argv[0] << " <config_file>" << std::endl;
         exit(1);
     }
+    string config_file = "./configs/default.conf";
+    if (argc == 2)
+        config_file = argv[1];
     try
     {
-        ConfigParser parser(argv[1]);
+        ConfigParser parser(config_file);
         vector<ServerConfig> configs = parser.parse();
         // configs[0].print();
         Server &webserv = Server::get_instance(configs[0], 10);
