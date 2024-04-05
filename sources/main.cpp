37276bc3d21623bf4860dff3984d355012569e5b
Taanviir <66136914+Taanviir@users.noreply.github.com>
2024-04-05 05:05:29 +0400

    chore: improved args handling in main

--- sources/main.cpp
+++ sources/main.cpp
@@ -5,15 +5,13 @@
 
 int main(int argc, char **argv) {
     if (argc > 2) {
-        std::cerr << "Usage 1: " << argv[0] << std::endl;
-        std::cerr << "Usage 2: " << argv[0] << " <config_file>" << std::endl;
+        std::cerr << "Usage: " << argv[0] << " [<config_file>]" << std::endl;
         exit(1);
     }
-    string config_file = "./configs/default.conf";
-    if (argc == 2)
-        config_file = argv[1];
+
     try
     {
+        string config_file = (argc == 2) ? argv[1] : "./configs/default.conf";
         ConfigParser parser(config_file);
         vector<ServerConfig> configs = parser.parse();
         // configs[0].print();
