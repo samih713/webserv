bffe474dabefe7cb001c745a4370c4a573e5f75f
samih713 <sakmohamed@gmail.com>
2024-04-14 05:21:43 +0400

    feat(Request): add new function `recv`
    - This is to handle partial receives, it can be called
     multiple times for the same request, it would append
     to rawRequest string. and notifiy when headers are ready.

--- sources/http/request/Request.hpp
+++ sources/http/request/Request.hpp
@@ -1,33 +1,61 @@
 #include "../Message.hpp"
-#include "../../../includes/webserv.hpp"
+#include "../TimeOut.hpp"
 
 #ifndef REQUEST_HPP
 #define REQUEST_HPP
+
+static const int BUFFER_SIZE(40);
+
+// should be mask bits
+static const int NOT_SET(-1);
+static const int NOT_SPECIFIED(0);
 
 // Request class to parse an incoming message
 class Request
 {
     public:
-        Request(const string &rawRequest);
+        Request();
         ~Request();
         Request(const Request &other);
-        METHOD          get_method() const;
-        const string   &get_resource() const;
-        const vsp      &get_headers() const;
+
+        int recv(fd socket);
+
+        METHOD        get_method() const;
+        const string &get_resource() const;
+        const vsp    &get_headers() const;
+
+        void parse_body();
+        void parse_header();
+
+        bool header_ready();
+        bool isParsed();
+        bool isCompleted();
+        void setCompleted();
+        int  expected_body_size();
+
+        TimeOut timer;
+
         friend ostream &operator<<(ostream &os, const Request &r);
 
     private:
-        void          parse();
-        const string &rawRequest;
-        // request-line   = method SP request-target SP HTTP-version
+        void   parse_content_length(const string &contentLength);
+        string rawRequest;
+
+        size_t headerEnd;
+        bool   headerReady;
+        bool   parsed;
+        bool   completed;
+        int    expectedBodySize;
+
+
         METHOD method;
         string resource;
         string http_version;
-        // field-line   = field-name ":" OWS field-value OWS
-        vsp header_fields;
-        vsp trailer_fields;
-        // clang-format on
+
+        vsp         header_fields;
+        vsp         trailer_fields;
         std::string body;
+
         // deleted copy assigment
         void operator=(const Request &);
 };
---
+++ sources/http/request/recv.cpp
@@ -0,0 +1,24 @@
+#include "../../server/Server.hpp"
+#include "Request.hpp"
+
+int Request::recv(fd socket)
+{
+    char buffer[BUFFER_SIZE] = { 0 };
+    int  bytesReceived;
+
+    bytesReceived = ::recv(socket, &buffer[0], BUFFER_SIZE - 1, 0);
+
+    if (bytesReceived == 0)
+        return CLOSE_CONNECTION;
+
+    if (bytesReceived == -1)
+    {
+        DEBUG_MSG("recieve failed:" + string(strerror(errno)), R);
+        return CLOSE_CONNECTION;
+    }
+
+    rawRequest.append(buffer, bytesReceived);
+    if (rawRequest.find(CRLF + CRLF) != string::npos)
+        headerReady = true;
+    return KEEP_ALIVE;
+}
