e0bd5d6056578923140f89744c0943143b7d4285
samih713 <sakmohamed@gmail.com>
2024-04-16 23:59:09 +0400

    chore: formatting changes

--- sources/http/TimeOut.hpp
+++ sources/http/TimeOut.hpp
@@ -4,25 +4,17 @@
 #define TIMOUT_HPP
 
 // default connection time out, this needs to come from the config
-static const time_t DEFAULT_TIMEOUT(1000);
+static const time_t DEFAULT_TIMEOUT(10);
 
-struct TimeOut
-{
-    public:
-        TimeOut()
-            : lastEvent(time(0)){};
-        void update_time()
-        {
-            lastEvent = time(0);
-        }
-        bool is_timeout()
-        {
-            return (time(0) - lastEvent) > timeOut;
-        };
+struct TimeOut {
+public:
+    TimeOut() : lastEvent(time(0)) {};
+    void update_time() { lastEvent = time(0); }
+    bool is_timeout() { return (time(0) - lastEvent) > timeOut; };
 
-    private:
-        const static time_t timeOut = DEFAULT_TIMEOUT;
-        time_t              lastEvent;
+private:
+    const static time_t timeOut = DEFAULT_TIMEOUT;
+    time_t              lastEvent;
 };
 
 #endif //  TIMOUT_HPP
