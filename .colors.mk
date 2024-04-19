3019254908f760481fedefe27c0de5b5a67116c8
Taanviir <66136914+Taanviir@users.noreply.github.com>
2024-04-19 15:47:10 +0400

    chore: added objects/ to gitignore and changed purple in .colors.mk

--- .colors.mk
+++ .colors.mk
@@ -7,6 +7,6 @@ RED=`tput bold setaf 1` # Red
 GREEN=`tput bold setaf 2` # Green
 YELLOW=`tput bold setaf 3` # Yellow
 BLUE=`tput bold setaf 4` # Blue
-PURPLE=`tput bold setaf 5` # Purple
+MAGENTA=`tput bold setaf 5` # Purple
 CYAN=`tput bold setaf 6` # Cyan
 WHITE=`tput bold setaf 7` # White
