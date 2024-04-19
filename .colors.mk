ccd4fbc0ef5b00e9568b65ae4b5476aa72b15ccc
Tanvir Ahmed Anas <tanvir.program@gmail.com>
2024-04-19 20:21:59 +0400

    chore: fixed Makefile colors for mac

--- .colors.mk
+++ .colors.mk
@@ -1,12 +1,27 @@
-# Reset
-RESET=`tput sgr0`
+ifeq ($(shell uname), Linux)
+# Reset colors
+RESET:=`tput sgr0`
 
 # Regular Colors
-BLACK=`tput bold setaf 0` # Black
-RED=`tput bold setaf 1` # Red
-GREEN=`tput bold setaf 2` # Green
-YELLOW=`tput bold setaf 3` # Yellow
-BLUE=`tput bold setaf 4` # Blue
-MAGENTA=`tput bold setaf 5` # Purple
-CYAN=`tput bold setaf 6` # Cyan
-WHITE=`tput bold setaf 7` # White
+RED:=`tput bold setaf 1`
+GREEN:=`tput bold setaf 2`
+YELLOW:=`tput bold setaf 3`
+BLUE:=`tput bold setaf 4`
+MAGENTA:=`tput bold setaf 5`
+CYAN:=`tput bold setaf 6`
+WHITE:=`tput bold setaf 7`
+
+else ifeq ($(shell uname), Darwin)
+# Reset colors
+RESET:=\033[0m
+
+# Regular Colors
+RED:=\033[1;31m
+GREEN:=\033[1;32m
+YELLOW:=\033[1;33m
+BLUE:=\033[1;34m
+MAGENTA:=\033[1;35m
+CYAN:=\033[1;36m
+WHITE:=\033[1;37m
+
+endif
