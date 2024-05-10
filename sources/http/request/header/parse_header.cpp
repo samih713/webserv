301a5ec2f1eb74deda1597ca867e5c99e882daa1
samih713 <sakmohamed@gmail.com>
2024-05-11 03:40:59 +0400

    chore(Header): move header files to header folder

--- sources/http/request/header/parse_header.cpp
+++ sources/http/request/header/parse_header.cpp
@@ -1,13 +1,11 @@
 #include "Header.hpp"
 #include "Message.hpp"
-#include "debug.hpp"
 #include "request_utils.hpp"
 #include "webserv.hpp"
 
 // helpers
 static void   replace_spaces(string& resource);
 static string get_field_value(stringstream& message);
-
 
 void Header::parse_header(stringstream& message)
 {
@@ -20,6 +18,7 @@ void Header::parse_header(stringstream& message)
     validate_terminator(message, CRLF);
     // after all headers are completed
     process_content();
+	//! unifiy error handling sometimes its set-state sometimes its set to BAD
     if (state == BAD)
         message.setstate(std::ios::failbit);
 }
@@ -33,11 +32,8 @@ void Header::parse_request_line(stringstream& message)
 
     std::noskipws(message);
     message >> method >> singleSpace[0] >> resource >> singleSpace[1] >> version;
-    if (strncmp(singleSpace, "  ", 2))
-        message.setstate(std::ios::failbit);
-    if (strncmp(acceptedVersion.c_str(), version.c_str(),
-            std::max(version.length(), acceptedVersion.length())))
-        message.setstate(std::ios::failbit);
+    if (strcmp(singleSpace, "  ") || acceptedVersion != version)
+		state = BAD;
     validate_terminator(message, CRLF);
     replace_spaces(resource);
 }
@@ -57,7 +53,7 @@ void Header::add_header(stringstream& message)
     to_lower(fieldName);
     fieldValue = get_field_value(message);
 
-    // set-cookie is a special case where it would be on different fields
+    //! set-cookie is a special case where it would be on different fields
     if (fields.find(fieldName) != fields.end())
         fields.at(fieldName).append(", " + fieldValue);
     else
@@ -70,7 +66,6 @@ void Header::process_content()
     const HeaderMap::iterator encoding      = fields.find("transfer-encoding");
     const HeaderMap::iterator contentLength = fields.find("content-length");
 
-    // if content-length && chunked is last ... etc do that whole logic here
     if (encoding != fields.end())
         is_chunked(encoding);
     else if (contentLength != fields.end())
@@ -79,20 +74,20 @@ void Header::process_content()
 
 void Header::is_chunked(const HeaderMap::const_iterator it)
 {
-    const string& tEncoding(it->second);
+    const string& transferEncoding(it->second);
     string        last;
     stack<string> values;
     string        word;
     bool          exists(false);
 
-    size_t delLoc(0);
+    size_t delPos(0);
     size_t i(0);
-    while (delLoc != string::npos) { // not at end
-        delLoc = tEncoding.find(", ", i);
-        word   = tEncoding.substr(i, delLoc - i);
+    while (delPos != string::npos) { // not at end
+        delPos = transferEncoding.find(", ", i);
+        word   = transferEncoding.substr(i, delPos - i);
         to_lower(word); // transfer encodings are case insensitive
         values.push(word);
-        i = delLoc + 2;
+        i = delPos + 2;
         if (word == "chunked")
             exists = true;
     }
@@ -101,23 +96,26 @@ void Header::is_chunked(const HeaderMap::const_iterator it)
     to_lower(last); // transfer encoding is case insensitive
     if (last == "chunked")
         chunked = true;
+    // if not set status to 400, size cannot be determined
     if (exists && !chunked)
         state = BAD;
-    // if not set status to 400
 }
 
 void Header::find_content_length(const HeaderMap::const_iterator it)
 {
-    if (it == fields.end())
+    if (it == fields.end()) {
         bodySize = NOT_SPECIFIED;
+		return ;
+	}
 
     istringstream length(it->second);
-
     length.exceptions(std::ios::failbit | std::ios::badbit);
     length >> bodySize;
 
-    if (!length.eof())
+    if (!length.eof()) {
         length.setstate(std::ios::failbit);
+		// DEBUG_MSG("failed here", R);
+	}
 }
 
 // helper utils
@@ -137,7 +135,6 @@ static string get_field_value(stringstream& message)
 
     return fieldValue;
 }
-
 
 // replace %20 with space
 // TODO replace all other special characters
