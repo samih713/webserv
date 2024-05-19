046a523af668eb67e6c18e6de0b949083afec06c
samih713 <sakmohamed@gmail.com>
2024-05-20 01:49:10 +0400

    build(http): refactor module.mk

--- sources/http/module.mk
+++ sources/http/module.mk
@@ -3,9 +3,13 @@
 REQUEST_DIR:=$(HTTP_DIR)/request
 REQUEST_SRCS:= $(wildcard $(REQUEST_DIR)/*.cpp) $(wildcard $(REQUEST_DIR)/header/*.cpp)
 
-HTTP_SRCS:= $(REQUEST_SRCS) $(HTTP_DIR)/response/Response.cpp $(HTTP_DIR)/handler/GetRequestHandler.cpp
-HTTP_OBJS:= $(HTTP_SRCS:$(SRCS_DIR)/%.cpp=$(OBJS_DIR)/%.o)
-OBJS += $(HTTP_OBJS)
+RESPONSE_DIR:=$(HTTP_DIR)/response
+RESPONSE_SRCS:= $(wildcard $(RESPONSE_DIR)/*.cpp)
+
+HANDLER_DIR:=$(HTTP_DIR)/handler
+HANDLER_SRCS:= $(wildcard $(HANDLER_DIR)/*.cpp)
+
+SRCS+= $(REQUEST_SRCS) $(RESPONSE_SRCS) $(HANDLER_SRCS)
 
 # tester mains
 TEST_HTTP:= $(HTTP_DIR)/test_http
