a176db6985453ba57af1b221d8c02998799aa6c4
samih713 <sakmohamed@gmail.com>
2024-05-29 22:07:13 +0400

    feat(GetRequestHandler): Implement CachedPages
    - Use CachedPages and generated error pages in the get handler

--- sources/http/handler/GetRequestHandler.cpp
+++ sources/http/handler/GetRequestHandler.cpp
@@ -4,87 +4,57 @@
 #include "debug.hpp"
 #include "webserv.hpp"
 
-GetRequestHandler::GetRequestHandler()
+// #define LOG_INFO(message)  Logger::log_message(message, "INFO")
+// #define LOG_DEBUG(message) Logger::log_message(message, "DEBUG")
+// #define LOG_ERROR(message) Logger::log_message(message, "ERROR")
+/* -------------------------------- Helpers -------------------------------- */
+
+Response GetRequestHandler::handle_request(const Request& r)
 {
-    DEBUG_MSG("GetRequestHandler constructor called", B);
+    LOG_INFO("Handling GET request ... ");
+    const vector<char>& body = get_resource(r);
+    return Response(status, responseHeaders, body);
 }
 
-GetRequestHandler::~GetRequestHandler()
+const vector<char> GetRequestHandler::get_resource(const Request& r)
 {
-    DEBUG_MSG("GetRequestHandler destructor called", B);
-};
+    status          = r.get_status();
+    string resource = r.get_resource();
 
-
-// TODO resource handling for get-requests, is broken
-const vector<char> GetRequestHandler::get_resource(const Request& request,
-    CachedPages& cached, const ServerConfig& config)
-{
-    // status                = request.get_status();
-    status                = INTERNAL_SERVER_ERROR;
-    string       resource = request.get_resource();
     vector<char> body;
-    // size_t       resource_size = 0;
 
-    add_header("Server", config.serverName);
+    add_header("Server", cfg.serverName);
 
-    //!
-    DEBUG_MSG(">>>>>>>>>", W);
-    DEBUG_MSG("Resource: " + resource, W);
-    DEBUG_MSG(">>>>>>>>>", W);
-    //!
-
-    // string       defaultPage = config.root + "/";
-
-    //* most likely an error
-    if (status >= 400) {
-        Page& p = cached.get_error_page(status);
-        body    = p.data;
+    // default path
+    if (resource == (cfg.root + "/")) {
+        Page& p = cp.get_page("index");
         add_header("Content-Type", p.contentType);
         add_header("Content-Length", ws_itoa(p.contentLength));
-        return body;
+        return p.data;
     }
 
-    // if (status != OK) { // TODO shouldn't just be errors
-    //     Page errorPage = get_error_page(status, cached, config);
-    //     // return errorPage;
-    // }
+    ifstream resource_file(resource.c_str(), std::ios_base::binary);
+    if (resource_file.fail())
+        status = NOT_FOUND; //! need more error handling, eg: forbidden, not allowed, etc
 
-    // a non implemented method is 501
-    //  a URI thats too long is 414
-    // if (resource == defaultPage) { //!
+    // errors
+    if (status >= 400)
+        return (get_error_body(status, cp));
 
-    //     body = cached.pages["home"].data;
-    //     add_header("Content-Type", cached.pages["home"].contentType.c_str());
-    //     add_header("Content-Length", ws_itoa(cached.pages["home"].contentLength));
-    // }
-
-    // ifstream resource_file(resource.c_str(), std::ios_base::binary);
-    // if (resource_file.fail()) {
-    //     // try to find error page
-    //     Page notFound = cached.pages[];
-    //     add_header("Content-Type", cached.pages["notFound"].contentType);
-    //     add_header("Content-Length",
-    //         ws_itoa(cached.pages["notFound"].contentLength));
-    // }
-    // else {
-    //     string resource_type = find_resource_type(resource);
-    //     if (resource_type.length() != 0)
-    //         add_header("Content-Type", resource_type);
-    //     if (resource.find("/cgi-bin") != string::npos) { //! cgi check again
-    //         CGI    cgi(request, config);
-    //         string result = cgi.execute();
-    //         body          = vector<char>(result.begin(), result.end());
-    //         add_header("Content-Length", ws_itoa(body.size()));
-    //     }
-    //     else {
-    //         body = vector<char>((std::istreambuf_iterator<char>(resource_file)),
-    //             std::istreambuf_iterator<char>());
-    //         resource_file.seekg(0, std::ios_base::end);
-    //         resource_size = resource_file.tellg();
-    //         add_header("Content-Length", ws_itoa(resource_size));
-    //         resource_file.seekg(0, std::ios_base::beg);
-    //     }
-    // }
+    string resource_type = find_resource_type(resource);
+    if (resource_type.length() != 0)
+        add_header("Content-Type", resource_type);
+    if (resource.find("/cgi-bin") != string::npos) { //! cgi check again
+        CGI    cgi(r, cfg);
+        string result = cgi.execute();
+        body          = vector<char>(result.begin(), result.end());
+        add_header("Content-Length", ws_itoa(body.size()));
+    }
+    else {
+        body = vector<char>((std::istreambuf_iterator<char>(resource_file)),
+            std::istreambuf_iterator<char>());
+        add_header("Content-Length", ws_itoa(body.size()));
+    }
 
     /* authentication function goes here for the requested resource */
     // DEBUG_MSG("Resource '" + resource + "' : [" + ws_itoa(resource_size) + "]", W);
@@ -98,13 +68,19 @@ const vector<char> GetRequestHandler::get_resource(const Request& request,
     return body;
 }
 
-
-Response GetRequestHandler::handle_request(const Request& request, CachedPages& cached,
-    const ServerConfig& config)
+/* ------------------------------------------------------------------------- */
+/*                                Constructor                                */
+/* ------------------------------------------------------------------------- */
+GetRequestHandler::GetRequestHandler(ServerConfig& cfg, CachedPages& cp)
+    : RequestHandlerBase(cfg, cp)
 {
-    DEBUG_MSG("Handling GET request ... ", B);
-
-    // ! reply to invalid requests
-    vector<char> body = get_resource(request, cached, config);
-    return Response(status, responseHeaders, body);
+    DEBUG_MSG("GetRequestHandler constructor called", B);
 }
+
+/* ------------------------------------------------------------------------- */
+/*                                 Destructor                                */
+/* ------------------------------------------------------------------------- */
+GetRequestHandler::~GetRequestHandler()
+{
+    DEBUG_MSG("GetRequestHandler destructor called", B);
+};
--- sources/http/handler/RequestHandlerBase.hpp
+++ sources/http/handler/RequestHandlerBase.hpp
@@ -15,7 +15,6 @@ public:
         responseHeaders.insert(make_pair(headerName, headerValue));
     }
 
-    //! ideally this should be done in Request itself
     const string find_resource_type(const string& resource)
     {
         size_t extension_index = resource.find_last_of('.');
@@ -24,8 +23,15 @@ public:
             file_extension = resource.substr(extension_index + 1);
         else
             file_extension = "";
-        //! need to protect against fail case
-        return fileTypes.find(file_extension)->second;
+        return fileTypes.find(file_extension)->second; //!
+    }
+
+    const vector<char>& get_error_body(STATUS_CODE status, CachedPages& cp)
+    {
+        Page& p = cp.get_error_page(status);
+        add_header("Content-Type", p.contentType);
+        add_header("Content-Length", ws_itoa(p.contentLength));
+        return p.data;
     }
 
 protected:
